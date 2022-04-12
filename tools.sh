360f23e1dcd55e241f60366eba8d621557b322a1
AlenIsmic <alen.ismic@gmail.com>
2022-04-12 19:07:44 +0200

    initial

--- tools.sh
+++ tools.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 
-brew install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+brew install --cask intellij-idea
+brew install warrensbox/tap/tfswitch
