600c5b0cb4d93405fe65606805409fcde2e98887
AlenIsmic <alen.ismic@gmail.com>
2022-04-12 19:42:13 +0200

    initial

--- zsh.sh
+++ zsh.sh
@@ -3,4 +3,5 @@
 brew install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
